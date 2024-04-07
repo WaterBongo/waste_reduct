301c9cf9a89d27ddbc2df2bb8c5e26b458f46a77
WaterBongo <cutegato@riseup.net>
2024-04-07 04:42:18 -0700

    cleaned up repos

--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-print("hi")
-r =requests.get("http://localhost:8080/product_stabilitiy/google/app_management/GOOG")
-print(r.text)
--- test2.py
+++
@@ -1,4 +0,0 @@
-import requests
-print("hi")
-r =requests.get("http://localhost:8052/get/Jane")
-print(r.text)
